5c808d854c2de481e27013cb022ca636b84112c6
9names <60134748+9names@users.noreply.github.com>
2024-09-01 15:42:41 +1000

    Add enter_xip and exit_xip commands

--- src/picousb.rs
+++ src/picousb.rs
@@ -460,6 +460,19 @@ impl<T: UsbContext> PicobootConnection<T> {
         self.cmd(cmd, vec![])
     }
 
+    #[allow(dead_code)]
+    pub fn enter_xip(&mut self) -> rusb::Result<()> {
+        let args = [0; 16];
+        let cmd = PicobootCmd::new(PicobootCmdId::EnterCmdXip, 0, 0, args);
+        Ok(self.cmd(cmd, vec![]).map(|_| ())?)
+    }
+
+    pub fn exit_xip(&mut self) -> rusb::Result<()> {
+        let args = [0; 16];
+        let cmd = PicobootCmd::new(PicobootCmdId::ExitXip, 0, 0, args);
+        Ok(self.cmd(cmd, vec![]).map(|_| ())?)
+    }
+
     pub fn reset_interface(&mut self) {
         self.handle
             .clear_halt(self.in_addr)
